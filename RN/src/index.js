975513fb9394a9a37c11a703a56db61a9e4f10ce
Ryan <ryan@Ryans-MacBook-Pro.local>
2017-05-06 00:35:24 +0800

    5.5

--- RN/src/index.js
+++ RN/src/index.js
@@ -5,14 +5,32 @@
  */
 
 import React, { Component } from "react";
-import { AppRegistry } from "react-native";
+import { AppRegistry, View, Text } from "react-native";
 import { StackNavigator } from "react-navigation";
 import { MainScreen, ProfileScreen } from "./containers";
+import Title from "./Screen/components/title/index";
+
+class RN extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center"
+        }}
+      >
+        <Title timeline="7分钟" />
+      </View>
+    );
+  }
+}
 
 export default function native(platform) {
-  const RN = StackNavigator({
-    Home: { screen: MainScreen },
-    Profile: { screen: ProfileScreen }
-  });
+  // const RN = StackNavigator({
+  //   Home: { screen: MainScreen },
+  //   Profile: { screen: ProfileScreen }
+  // });
   AppRegistry.registerComponent("RN", () => RN);
 }
